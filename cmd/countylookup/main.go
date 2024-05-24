1771f5579c8c31c44fcd61a711803edf18ad48ee
Tamara deMent <79533597+tamaradement@users.noreply.github.com>
2024-05-24 12:16:55 -0400

    Add executable to extract and process ZIP+4 details, grouping zip codes with county codes (#2)
    * Adds initial implementation for ZIP+4 Query Tool
    
    Adds an executable that utilizes the zip4 package to extract zip4Details from a valid tar file; groups five digit zips with all corresponding county codes and outputs to the console.
    
    Tested:
    - Ran the program with a valid tar file and observed the output.
    - Verified error messages for missing tar file argument.
    - Verified error messages for non-existent tar file.
    
    * Improve error handling and function naming for clarity
    
    This commit updates the error handling to use fmt.Fprintf + os.Exit(1) pattern for better control over output. Additionally, it renames the function CollectDetails to the more descriptive CollectZip4Details.
    
    Testing:
    - Ran the program with a valid tar file and verified correct output.
    - Checked error messages for scenarios with a missing tar file argument.
    - Checked error messages for scenarios with an invalid tar file.
    
    * Optimize memory usage of counties logic
    
    Refactored the logic within the yield function to directly update zip-to-county mappings as data is read.
    Additionally, moved the 'counties' package to the 'internal' directory to restrict its accessibility outside the module.
    Removed the redundant existence check for zip codes in the map,
    
    Tested:
        - Ran the program with a valid tar file and observed output.
        - Checked error messages for scenarios with a missing tar file argument.
        - Checked error messages for scenarios with an invalid tar file.
    
    ---------
    
    Co-authored-by: Tamara Dement <tamaradement@Tamaras-Laptop.local>

---
+++ cmd/countylookup/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/corbaltcode/usps/internal/counties"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Error: Missing tar file name\nUsage: %s <tar_file_name>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	tarName := os.Args[1]
+
+	zipPassword := mustGetenv("ZIP_PASSWORD")
+
+	zipToCounty, err := counties.CollectZip4Details(tarName, zipPassword)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to collect details: %v\n", err)
+		os.Exit(1)
+	}
+
+	for zip, counties := range zipToCounty {
+		fmt.Printf("ZIP Code: %s, County Numbers: %v\n", zip, counties)
+	}
+}
+
+func mustGetenv(key string) string {
+	v, ok := os.LookupEnv(key)
+	if !ok {
+		panic(fmt.Sprintf("missing env var: %v", key))
+	}
+	return v
+}
